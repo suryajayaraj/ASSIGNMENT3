51b2655264dbc36b97ec2d304192a9dc65671424
suryajayaraj <43784796+suryajayaraj@users.noreply.github.com>
2025-04-22 19:38:58 +0530

    Add files via upload

---
+++ pgm1.py
@@ -0,0 +1,10 @@
+
+def calculator(a):
+    import math
+    print("squareroot:",(math.sqrt(a)))
+    print("logarithm:",(math.log(a)))
+    print("sine:",(math.sin(a)))
+calculator(int(input("enter a number:")))
+
+
+
